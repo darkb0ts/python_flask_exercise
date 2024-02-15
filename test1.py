7c79264994fa1d9bf0bed9cf7fb126acf31969b0
darkb0ts <prasanna110600@gmail.com>
2024-02-15 18:05:40 +0530

    flask test1 adding route

---
+++ test1.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+	return 'Hello World'
+
+@app.route('/name')
+def second():
+    return "prasanna"
+
+# main driver function
+if __name__ == '__main__':
+
+	# run() method of Flask class runs the application 
+	# on the local development server.
+	app.run()
